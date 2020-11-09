497edb5a00873a8f9c605618536be357500c3e34
Eugeniu Racila <eugeniu.racila@gmail.com>
2020-11-09 15:05:07 +0100

    Attempt to fix hub dispose invocation

---
+++ LogicLayer/Hubs/EventHub.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LogicLayer.Hubs
+{
+    public class EventHub
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly string baseUrl = "https://jsonplaceholder.typicode.com/posts";
+        
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public EventHub(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        
+        public async void SendNotification(string message)
+        {
+            Console.WriteLine("SendNotification was called by the client");
+            await _hubContext.Clients.All.SendAsync("notification", message);
+        }
+        
+        public async void GetOrders()
+        {
+            HttpResponseMessage httpResponseMessage = await _client.GetAsync(baseUrl);
+            
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine("Response 200");
+                Console.WriteLine(responseContent);
+
+                await _hubContext.Clients.All.SendAsync("receiveOrders", responseContent);
+
+            }
+            else
+            {
+                Console.WriteLine("Response not 200");
+            }
+        }
+        // here place some method(s) for message from client to server
+    }
+}
--- LogicLayer/NotificationHub.cs
+++ LogicLayer/NotificationHub.cs
@@ -7,34 +7,10 @@ namespace LogicLayer
 {
     public class NotificationHub : Hub
     {
-        private readonly HttpClient _client = new HttpClient();
-        private readonly string baseUrl = "https://jsonplaceholder.typicode.com/posts";
-        
         public override Task OnConnectedAsync()
         {
             Console.WriteLine($"New connection established :: {Context.ConnectionId}");
             return base.OnConnectedAsync();
-        }
-
-        public async void SendNotification(string message)
-        {
-            await Clients.All.SendAsync("notification", message);
-        }
-
-        public async void GetOrders()
-        {
-            await Clients.All.SendAsync("notification", "GetOrders - before http request");
-            HttpResponseMessage httpResponseMessage = await _client.GetAsync(baseUrl);
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Response 200");
-        
-                await Clients.All.SendAsync("notification", "GetOrders - after http request");
-            }
-            else
-            {
-                Console.WriteLine("Response not 200");
-            }
         }
     }
 }
